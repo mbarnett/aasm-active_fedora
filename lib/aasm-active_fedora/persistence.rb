536be6f81a893b0dce8bf44416e078286c016de1
Matt Barnett <matt@sixtyodd.com>
2017-08-28 15:12:52 -0600

    Fix module prepending

--- lib/aasm-active_fedora/persistence.rb
+++ lib/aasm-active_fedora/persistence.rb
@@ -1,11 +1,20 @@
+require 'pry'
 module AASM
   module PersistenceWithActiveFedora
-    def load_persistence(base)
-      hierarchy = base.ancestors.map {|klass| klass.to_s}
-      if hierarchy.include?('ActiveFedora::Base')
-        include_persistence base, :active_fedora
-      else
-        super
+    module ClassMethods
+      def load_persistence(base)
+        hierarchy = base.ancestors.map {|klass| klass.to_s}
+        if hierarchy.include?('ActiveFedora::Base')
+          include_persistence base, :active_fedora
+        else
+          super
+        end
+      end
+    end
+
+    def self.prepended(base)
+      class << base
+        prepend ClassMethods
       end
     end
   end
--- lib/aasm-active_fedora/version.rb
+++ lib/aasm-active_fedora/version.rb
@@ -1,5 +1,5 @@
 module Aasm
   module ActiveFedora
-    VERSION = "0.1.2"
+    VERSION = "0.2.0.pre"
   end
 end
