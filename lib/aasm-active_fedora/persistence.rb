c278632c8dd11cf70c62bd4c1edf580d2ae133af
Matt Barnett <matt@sixtyodd.com>
2017-08-28 13:32:19 -0600

    Ditch deprecated patch

--- lib/aasm-active_fedora/persistence.rb
+++ lib/aasm-active_fedora/persistence.rb
@@ -1,16 +1,14 @@
 module AASM
-  module Persistence
-    class << self
-      def load_persistence_with_active_fedora(base)
-        hierarchy = base.ancestors.map {|klass| klass.to_s}
-        if hierarchy.include?('ActiveFedora::Base')
-          include_persistence base, :active_fedora
-        else
-          load_persistence_without_active_fedora(base)
-        end
+  module PersistenceWithActiveFedora
+    def load_persistence(base)
+      hierarchy = base.ancestors.map {|klass| klass.to_s}
+      if hierarchy.include?('ActiveFedora::Base')
+        include_persistence base, :active_fedora
+      else
+        super
       end
-
-      alias_method_chain :load_persistence, :active_fedora
     end
   end
 end
+
+AASM::Persistence.send(:prepend, AASM::PersistenceWithActiveFedora)
